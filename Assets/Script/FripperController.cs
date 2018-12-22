2ce8655d48568ca4b86eebb2067ce7ae29e6ac7a
sitmn <sho-t-jpx375@docomo.ne.jp>
2018-12-22 19:10:23 +0900

    Lesson5 課題と発展課題
    課題：合計スコアをBallControllerに宣言、スコアの加算は新たにScoreControllerを追加
    発展課題：BallControllerのUpdate関数に、矢印キーと同様に作成

---
+++ Assets/Script/FripperController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+    private HingeJoint myHingeJoint;
+
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+
+    private int rightid, leftid;
+
+
+    // Use this for initialization
+    void Start () {
+        
+    myHingeJoint = GetComponent<HingeJoint>();
+
+        SetAngle(this.defaultAngle);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+		if(Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        
+        if(Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if(Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        //追加課題
+        if(Input.touchCount > 0)
+        {
+            foreach(Touch touch in Input.touches)
+            {
+                
+                
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        if (touch.position.x >= 286 && tag == "RightFripperTag") 
+                        {
+                            SetAngle(this.flickAngle);
+                            rightid = touch.fingerId;
+
+                        }else if(touch.position.x < 286 && tag == "LeftFripperTag")
+                        {
+                            SetAngle(this.flickAngle);
+                            leftid = touch.fingerId;
+                        }
+                        break;
+
+
+                    case TouchPhase.Ended:
+                        if (tag == "RightFripperTag" && rightid == touch.fingerId)
+                        {
+                            SetAngle(this.defaultAngle);
+                        }
+                        else if (tag == "LeftFripperTag" && leftid == touch.fingerId)
+                        {
+                            SetAngle(this.defaultAngle);
+                        }
+                        break;
+                }
+            }
+        }
+
+	}
+
+    void SetAngle(float angle)
+    {
+        JointSpring JointSpr = this.myHingeJoint.spring;
+        JointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = JointSpr;
+    }
+}
---
+++ Assets/Script/ScoreController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    private int point;
+    
+    private int smallstarhit = 50;
+    private int largestarhit = 20;
+    private int smallcloudhit = 10;
+    private int largecloudhit = 80;
+    
+    private GameObject scorepoint;
+    private GameObject ball;
+
+    private BallController ballcontroller;
+    private Text scoretext;
+   
+    
+    // Use this for initialization
+    void Start () {
+        this.ball = GameObject.Find("Ball");
+        this.ballcontroller = this.ball.GetComponent<BallController>();
+    
+        this.scorepoint = GameObject.Find("Scorepoint");
+        this.scoretext = this.scorepoint.GetComponent<Text>();
+
+        //タグ別にポイントを代入
+        if (tag == "SmallStarTag")
+        {
+            this.point = smallstarhit;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.point = largestarhit;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            this.point = smallcloudhit;
+        }else if(tag == "LargeCloudTag")
+        {
+            this.point = largecloudhit;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        ballcontroller.score += this.point;
+        this.scoretext.text = "score"+ ballcontroller.score;
+    }
+}
